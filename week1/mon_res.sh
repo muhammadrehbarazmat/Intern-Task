9a93ddc08f9f2fe325f3fb1ca82d06b23491b255
Muhammad Rehbar <muhammadrehbarazmat@gmail.com>
2026-04-02 19:04:38 +0500

    updated service restarted with email alert

--- week1/mon_res.sh
+++ week1/mon_res.sh
@@ -13,9 +13,12 @@ else
     if [ "$MEMORY" -gt "$LIMIT" ]; then
         echo "Memory usage is $MEMORY KB. Restarting $SERVICE_NAME..."
         sudo systemctl restart $SERVICE_NAME
+
         #Send email
-        echo "$MESSAGE" | mail -s "Nginx Restart Alert" "$Email"
-    else
+        echo "$SERVICE_NAME Service restarted successfully" | mail -s "Nginx Restart Alert" "$EMAIL"
+   
+       echo "service restarted successfully"
+        else
         echo "Memory usage is normal: $MEMORY KB"
     fi
 fi
