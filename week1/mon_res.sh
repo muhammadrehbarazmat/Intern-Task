0ede73b26722572807510447a096d342fea98483
Muhammad Rehbar <muhammadrehbarazmat@gmail.com>
2026-04-02 16:25:19 +0500

    Added monitoring script with email alert

--- week1/mon_res.sh
+++ week1/mon_res.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 SERVICE_NAME="nginx"
-LIMIT=256000   # 256 MB in KB
-Email=muhammadrehbarazmat@gmail.com
+LIMIT=1000   # 256 MB in KB
+EMAIL="muhammadrehbarazmat@gmail.com"
+
 
 # Get memory usage of the process
 MEMORY=$(ps -C $SERVICE_NAME -o rss= | awk '{sum+=$1} END {print sum}')
@@ -13,8 +13,8 @@ else
     if [ "$MEMORY" -gt "$LIMIT" ]; then
         echo "Memory usage is $MEMORY KB. Restarting $SERVICE_NAME..."
         sudo systemctl restart $SERVICE_NAME
-        #send email
-        echo "$MESSAGE" | mail -s "Service Restart Alert" $EMAIL
+        #Send email
+        echo "$MESSAGE" | mail -s "Nginx Restart Alert" "$Email"
     else
         echo "Memory usage is normal: $MEMORY KB"
     fi
