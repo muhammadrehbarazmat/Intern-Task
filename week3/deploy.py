8669dbb35989c1830544d668f0efb8280e8672ca
Muhammad Rehbar <muhammadrehbarazmat@gmail.com>
2026-04-14 14:38:46 +0500

    updating deployment script

---
+++ week3/deploy.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+# CONFIG
+PROJECT_DIR = os.path.expanduser("~/internship-tasks/week4/myapp")
+BRANCH = "main"
+APP_FILE = "app.py"
+
+# Getting into project directory
+os.chdir(PROJECT_DIR)
+
+# Pulling latest code
+print("Pulling latest code...")
+subprocess.run(["git", "pull", "origin", BRANCH])
+
+#  Killing old app (if running)
+print("Stopping old app...")
+subprocess.run(["pkill", "-f", APP_FILE])
+
+#  Starting app in background
+print("Starting app...")
+subprocess.Popen(
+    ["nohup", "python3", APP_FILE],
+    stdout=open("output.log", "a"),
+    stderr=open("error.log", "a")
+)
+
+print("Deployment completed ✅")
